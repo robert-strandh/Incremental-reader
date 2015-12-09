7eb3deac207dab505a6a5f6a2362ded7ec403f5b
Robert Strandh <robert.strandh@gmail.com>
2015-12-09 11:13:10 +0100

    New function for updating the stack according to a state list designator.

--- stack.lisp
+++ stack.lisp
@@ -3,3 +3,12 @@
 ;;; The stack is represented as a list of instances of (subclasses of)
 ;;; the class STATE.
 (defparameter *stack* '())
+
+(defun update-stack (state-list-designator)
+  (cond ((null state-list-designator)
+	 nil)
+	((listp state-list-designator)
+	 (setf *stack* (append state-list-designator *stack*)))
+	(t
+	 (push state-list-designator *stack*)))
+  *stack*)
