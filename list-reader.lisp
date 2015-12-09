5729cc6b0ecfbc962b207293f814181a2f6b8f2a
Robert Strandh <robert.strandh@gmail.com>
2015-12-09 08:34:38 +0100

    New component for reading lists.

---
+++ list-reader.lisp
@@ -0,0 +1,25 @@
+(cl:in-package #:incremental-reader)
+
+(defclass open-parenthesis (terminating-macro)
+  ())
+
+(defclass closing-parenthesis (terminating-macro)
+  ())
+
+(defclass list-reader (state)
+  ((%elements :initform '() :initarg :elements :reader elements)))
+
+(defmethod clone ((state list-reader))
+  (make-instance (class-of state)
+    :elements (elements state)))
+
+(defmethod macro-character-state ((syntax-type open-parenthesis))
+  (make-instance 'list-reader))
+
+(defmethod process ((state list-reader) syntax-type)
+  (values (list state (make-instance 'initial-state))
+	  t))
+
+(defmethod process ((state list-reader) (syntax-type closing-parenthesis))
+  (setf *return-value* (list (reverse (elements state))))
+  '())
